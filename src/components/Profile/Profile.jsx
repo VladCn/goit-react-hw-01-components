ed7b825d9bd755bc8b8e295618de0b5d941dcb65
Vladislav Zaitsev <vladislav.zaytcev84@gmail.com>
2022-02-08 13:43:52 +0200

    End Profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,31 +1,63 @@
+import styled, { css } from 'styled-components'
+
+const ProfileWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const Avatar = styled.img`
+  width: auto;
+`
+const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const Stats = styled.ul`
+  display: flex;
+  padding: 0;
+`
+const Item = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  list-style-type: none;
+  padding: 20px;
+  border: 1px solid grey;
+  font-size: 24px;
+  text-transform: none;
+  width: 90px;
+  height: 90px;
+`
+
+
 export function Profile({ username, tag, location, avatar, stats }){
   return (
-    <div className="profile">
-      <div className="description">
-        <img
+    <ProfileWrapper>
+      <Description>
+        <Avatar
           src={avatar}
           alt="User avatar"
-          className="avatar"
         />
         <p className="name">{username}</p>
         <p className="tag">{tag}</p>
         <p className="location">{location}</p>
-      </div>
+      </Description>
 
-      <ul className="stats">
-        <li>
+      <Stats>
+        <Item>
           <span className="label">Followers</span>
           <span className="quantity">{stats.followers}</span>
-        </li>
-        <li>
+        </Item>
+        <Item>
           <span className="label">Views</span>
           <span className="quantity">{stats.views}</span>
-        </li>
-        <li>
+        </Item>
+        <Item>
           <span className="label">Likes</span>
           <span className="quantity">{stats.likes}</span>
-        </li>
-      </ul>
-    </div>
+        </Item>
+      </Stats>
+    </ProfileWrapper>
   )
 }
