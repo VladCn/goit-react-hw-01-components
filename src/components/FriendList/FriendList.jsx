f94a747cac28df207b719ced6adc1226cda0d746
VladCn <vladislav.zaytcev84@gmail.com>
2022-02-09 11:56:14 +0200

    create FriendList component

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,45 @@
+import styled, { css } from 'styled-components'
+
+const Item = styled.li`
+  display: flex;
+  align-items: center;
+  border: 1px solid black;
+  margin-bottom: 10px;
+  width: 400px;
+  padding: 20px;
+  & img, span {
+    margin-right: 10px;
+  }
+  & p {
+    margin: 0;
+  }
+`
+const Status = styled.span`
+  width: 20px;
+  height: 20px;
+  border: 1px solid black;
+  border-radius: 90px;
+  background-color: red;
+
+  ${props => props.status && css`
+    background: green;
+  `}
+`
+
+
+export function FriendList({ friends }){
+
+  return (
+    <ul className="friend-list">
+      {friends.map(item => {
+        return(
+          <Item key={item.id}>
+            <Status status={item.isOnline}/>
+            <img className="avatar" src={item.avatar} alt="User avatar" width="48" />
+            <p className="name">{item.name}</p>
+          </Item>
+        )
+      })}
+    </ul>
+  )
+}
